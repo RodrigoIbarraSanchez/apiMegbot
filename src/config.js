f7c399556d9010532fd89f81f1e0d23204e195e2
RodrigoIbarraSanchez <lookatthemoon@live.com.mx>
2017-05-06 17:18:23 -0500

    cambiando url completa de mongodb

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 module.exports = {
     port: 5000,
+    mongo_url: 'mongodb://heroku_62f7g21n:tnjf7910f72pki8or18uohkp8r@ds131041.mlab.com:31041/heroku_62f7g21n'
     //port: process.env.PORT || 5000,
     //mongo_url: process.env.MONGODB_URI
-    mongo_url: 'mongodb://localhost:27017/pruebamongoose'
+    //mongo_url: 'mongodb://localhost:27017/pruebamongoose'
 }
