4313d636779a279020d980b3bcddda030e0cdb38
RodrigoIbarraSanchez <lookatthemoon@live.com.mx>
2017-05-06 17:09:05 -0500

    BD local

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
 module.exports = {
-    port: process.env.PORT || 5000,
-    mongo_url: process.env.MONGODB_URI
+    port: 5000,
+    //port: process.env.PORT || 5000,
+    //mongo_url: process.env.MONGODB_URI
+    mongo_url: 'mongodb://localhost:27017/pruebamongoose'
 }
